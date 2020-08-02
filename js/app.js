22207d09b2ec911ee5b3ffa2bf936a25f631e864
Diego97-webdev <shizusenpai97@gmail.com>
2020-08-02 17:19:32 -0300

    archivos basicos, estructura html principal

---
+++ js/app.js
@@ -0,0 +1,40 @@
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".swiper-container-dos", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+});
+
+var swiper = new Swiper(".swiper-container-tres", {
+  speed: 600,
+  parallax: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
