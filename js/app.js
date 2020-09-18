265f45b63d6c09b496c6ba7af764ebcf2da7fb2c
Diego97-webdev <shizusenpai97@gmail.com>
2020-09-18 15:54:48 -0300

    agregado el titulo principal al hero, arreglada la primer galeria

--- js/app.js
+++ js/app.js
@@ -101,3 +101,54 @@ L.marker([-32.910181, -68.846877])
   .addTo(map)
   .bindPopup("CASA NRO-01")
   .openPopup();
+
+//MODAL
+
+const imgsGaleria = document.querySelectorAll(".imagenes-galeria");
+const modal = document.querySelector(".contenedor-modal");
+const imgModal = document.querySelector(".img-modal");
+const btnIzquierda = document.querySelector(".btn-izquierda");
+const btnDerecha = document.querySelector(".btn-derecha");
+const btnCerrarModal = document.querySelector(".btn-cerrar");
+
+let contador = 0;
+
+imgsGaleria.forEach((img) => {
+  img.addEventListener("click", () => {
+    modal.style.display = "flex";
+    let imagenSeleccionada = img.src;
+    imgModal.src = imagenSeleccionada;
+    let contador = Number(img.dataset.numero);
+  });
+});
+console.log(contador);
+
+btnIzquierda.addEventListener("click", () => {
+  console.log(contador);
+  contador--;
+  if (contador < 1) {
+    contador = 6;
+  }
+
+  imgModal.src = `/imagenes/galeria/casa-galeria-0${contador}.webp`;
+});
+
+btnDerecha.addEventListener("click", () => {
+  console.log(contador);
+  contador++;
+  if (contador > 6) {
+    contador = 1;
+  }
+
+  imgModal.src = `/imagenes/galeria/casa-galeria-0${contador}.webp`;
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target.classList == "contenedor-modal") {
+    modal.style.display = "none";
+  }
+}); //PARA CERRAR EL MODAL SOLO AL PRESIONAR EL AREA OSCURA
+
+btnCerrarModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
